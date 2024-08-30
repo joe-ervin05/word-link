0e3fdad0c9812e0590ba157e4d9d3457f9bb70c6
Joe Ervin <joeervin@Joes-Laptop.local>
2024-08-30 00:18:13 -0400

    init

---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+
+	wrdList, err := readLines("words.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	wrds, err := loadGraph()
+
+	if err != nil {
+		err = _initGraph()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		wrds, err = loadGraph()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	idx1, idx2 := getInp(wrdList)
+
+	for idx1 == idx2 {
+		fmt.Println("cannot link a word to itself")
+
+		idx1, idx2 = getInp(wrdList)
+	}
+
+	path := wrds.shortestPath(idx1, idx2)
+
+	if path == nil {
+		fmt.Printf("%s and %s cannot be linked\n", wrdList[idx1], wrdList[idx2])
+		return
+	}
+
+	for i := len(path) - 1; i >= 0; i-- {
+		fmt.Printf(" %s", wrdList[path[i]])
+		if i > 0 {
+			fmt.Printf(" ->")
+		}
+	}
+
+}
+
+func getInp(wrdList []string) (idx1 int, idx2 int) {
+
+	var wrd1 string
+
+	fmt.Print("from word: ")
+	fmt.Scan(&wrd1)
+
+	idx1 = indexOf(wrdList, wrd1)
+
+	for idx1 < 0 {
+		if len(wrd1) != 5 {
+			fmt.Print("your word must be five letters")
+		} else {
+			fmt.Print("word not found")
+		}
+
+		fmt.Print("\nfrom word: ")
+
+		fmt.Scan(&wrd1)
+		idx1 = indexOf(wrdList, wrd1)
+	}
+
+	var wrd2 string
+
+	fmt.Print("to word: ")
+	fmt.Scan(&wrd2)
+
+	idx2 = indexOf(wrdList, wrd2)
+
+	for idx2 < 0 {
+		if len(wrd2) != 5 {
+			fmt.Print("your word must be five letters")
+		} else {
+			fmt.Print("word not found")
+		}
+
+		fmt.Print("\nto word: ")
+
+		fmt.Scan(&wrd2)
+		idx2 = indexOf(wrdList, wrd2)
+	}
+
+	return idx1, idx2
+}
---
+++ words.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/gob"
+	"math"
+	"os"
+)
+
+const WORD_COUNT = 5757
+
+type queue []int
+type wordGraph [WORD_COUNT][]int
+
+// takes in a queue and returns the top item
+func top(q queue) int {
+	return q[0]
+}
+
+func discard(q queue) queue {
+	return q[1:]
+}
+
+func isEmpty(q queue) bool {
+	return len(q) == 0
+}
+
+// preforms a bredth-first search starting at the first word index
+// returns arrays for each words parent and their distance to the starting node
+func (wg wordGraph) bfs(from int) (par, dist []int) {
+	q := queue{from}
+
+	dist = make([]int, WORD_COUNT)
+	par = make([]int, WORD_COUNT)
+
+	for i := 0; i < WORD_COUNT; i++ {
+		par[i] = -1
+		dist[i] = math.MaxInt32
+	}
+
+	dist[from] = 0
+
+	q = append(q, from)
+
+	for !isEmpty(q) {
+
+		node := top(q)
+		q = discard(q)
+
+		for _, wrd := range wg[node] {
+			if dist[wrd] == math.MaxInt32 {
+				par[wrd] = node
+				dist[wrd] = dist[node] + 1
+
+				q = append(q, wrd)
+			}
+		}
+	}
+
+	return par, dist
+
+}
+
+// takes in a start word index and destination word index
+// returns an array with the indexes for the shortest path between the two words
+func (wg wordGraph) shortestPath(s, d int) []int {
+	par, dist := wg.bfs(s)
+
+	if dist[d] == math.MaxInt32 {
+		return nil
+	}
+
+	path := []int{d}
+
+	currNode := d
+
+	for par[currNode] != -1 {
+		path = append(path, par[currNode])
+		currNode = par[currNode]
+	}
+
+	return path
+}
+
+// returns a graph of word indexes and the words they are linked to
+func loadGraph() (wrdGraph wordGraph, err error) {
+	data, err := os.ReadFile("wordgraph.gob")
+
+	if err != nil {
+		return wrdGraph, err
+	}
+
+	buf := bytes.NewBuffer(data)
+	dec := gob.NewDecoder(buf)
+
+	err = dec.Decode(&wrdGraph)
+
+	return wrdGraph, err
+}
+
+func _initGraph() error {
+	wrds, err := readLines("words.txt")
+
+	if err != nil {
+		return err
+	}
+
+	wrdGraph := wordGraph{}
+
+	for i := 0; i < len(wrds); i++ {
+		for j := 0; j < len(wrds); j++ {
+
+			if misses(wrds[i], wrds[j]) == 1 {
+				wrdGraph[i] = append(wrdGraph[i], j)
+			}
+
+		}
+
+	}
+
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+
+	err = enc.Encode(wrdGraph)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile("wordgraph.gob", buf.Bytes(), 0644)
+
+}
+
+func misses(wrd1, wrd2 string) (m int) {
+	for i := 0; i < len(wrd1); i++ {
+		if wrd1[i] != wrd2[i] {
+			m++
+		}
+	}
+
+	return m
+}
+
+func readLines(path string) (lines []string, err error) {
+	file, err := os.Open(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	scn := bufio.NewScanner(file)
+
+	for scn.Scan() {
+		lines = append(lines, scn.Text())
+	}
+
+	if scn.Err() != nil {
+		return nil, err
+	}
+
+	return lines, nil
+}
+
+func indexOf(list []string, str string) int {
+
+	low := 0
+	high := WORD_COUNT - 1
+
+	for low <= high {
+		mid := low + (high-low)/2
+
+		if list[mid] == str {
+			return mid
+		}
+
+		if list[mid] < str {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+
+	return -1
+}
